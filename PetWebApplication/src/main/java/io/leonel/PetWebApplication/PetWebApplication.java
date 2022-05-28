d7c703b5ab6d7dbdf0a41d6c0e3ccbc663840300
leobarrientos02 <leobarrientos02@gmail.com>
2022-05-27 20:16:19 -0400

    first commit

---
+++ PetWebApplication/src/main/java/io/leonel/PetWebApplication/PetWebApplication.java
@@ -0,0 +1,13 @@
+package io.leonel.PetWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetWebApplication.class, args);
+	}
+
+}
---
+++ PetWebApplication/src/test/java/io/leonel/PetWebApplication/PetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.leonel.PetWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
