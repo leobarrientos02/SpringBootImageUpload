9a2d97099b08764af87342502d88419ded00f12b
leobarrientos02 <leobarrientos02@gmail.com>
2022-05-28 20:16:23 -0400

    angular components

--- UI/my-pet-app/src/app/app.component.ts
+++ UI/my-pet-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FileService } from './file.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-pet-app';
+
+  public filenames: string[] = [];
+  public fileStatus = { status: "", requestType: "", percent: 0};
+
+  constructor(private fileService: FileService){}
+
+  // Define a function to upload files
+  public onUploadFiles(files: File[]): void {
+    const formData = new FormData();
+    for(const file of files){
+      formData.append('files', file, file.name);
+    }
+    this.fileService.upload(formData).subscribe(
+      event => {
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+  // Define a function to download files
+  public onDownloadFiles(filename: string): void {
+    this.fileService.download(filename).subscribe(
+      event => {
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private reportProgress(httpEvent: HttpEvent<string[] | Blob>): void {
+    switch(httpEvent.type){
+      case HttpEventType.UploadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, 'Uploading');
+        break;
+      case HttpEventType.DownloadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, 'Downloading');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log("Header returned", httpEvent);
+        break;
+      case HttpEventType.Response:
+        if(httpEvent.body instanceof Array){
+          for(const filename of httpEvent.body){
+            this.filenames.unshift(filename);
+          }
+        } else {
+          // download logic
+          saveAs(new File([httpEvent.body!], httpEvent.headers.get('File-Name')!,
+            {type: `${httpEvent.headers.get('Content-Type')};charset=utf-8`}));
+        }
+        break;
+        default:
+          console.log(httpEvent);
+          break;
+    }
+  }
+
+  private updateStatus(loaded: number, total: number, requestType: string) {
+    this.fileStatus.status = 'progress';
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = Math.round(100 * loaded / total);
+  }
 }
